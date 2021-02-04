946d2eaa9111bbda896499946dd7307ca36655d7
Mary Cohan <mary.cohan@gmail.com>
2021-02-03 22:46:16 -0800

    working on current weather card

--- assets/script.js
+++ assets/script.js
@@ -1,80 +1,128 @@
 
 $(document).ready(function() {
 
-    // var searchValue = $("#searchInput").val();
-  
+
+
+
+ 
 
     $("#searchBtn").on("click", function(event) {
         event.preventDefault()
 
-        var searchValue = $("#searchInput").val();
+    
+        searchValue = ""
+    var searchValue = $("#searchInput").val();
         console.log(searchValue);
 
-        //function to get data from API
         if (searchValue === "") {
             return
         }
         else {
+    //pull up past searches 
+    var pastSearches = JSON.parse(localStorage.getItem("#pastSearch"));    
+    $("#pastSearch").append("<ul>"+ pastSearches + "</ul>")
+
+    console.log(pastSearches)
+    
+    //function to diplay history
+    function displayHistory() {
+    //Use local stoage to save weather search
+     localStorage.setItem("#pastSearch", JSON.stringify(searchValue))
+    }
+
+    
+        //display History
+        displayHistory()
         getApi()
         }
       // clear input box
-      $("#searchInput").val("");
+      $("#searchInput").val("")
+
+    
+    
     
 
-
+//function to get data from the API used to get lat/lon
     function getApi () {
 
-        var APIkey = "5a9bcc7ae1c44f294dec0c1395fed462";
-        // var requestUrl =  "http://api.openweathermap.org";
-
+        var APIkey = "5a9bcc7ae1c44f294dec0c1395fed462";      
         var apiSearchCity = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=" + APIkey;
         
+
 
         fetch(apiSearchCity)
         .then(function (response) {
            return response.json()
         })
         .then(function (data){
-            // $("#searchResults").text(JSON.stringify(data))
-            $("#searchResults").append("<h2>" + "Weather Details of...." + data.city.name + "</h2>")
-            $("#searchResults").append("<h2>" + data.list[0].weather[0].main + "</h2>") 
-            $("#searchResults").append("<h2>" + data.list[0].weather[0].description + "</h2>") 
 
-            //weather icon
-            var iconcode = data.list[0].weather[0].icon;
-            var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-            $('#wicon').attr('src', iconurl);
-            
-
-            //get date???
-            var d = new Date();
-            var n = d.getDate();
-
-            $("#searchResults").append("<h2>" + n + "</h2>") 
-
-                // $("#searchResults").append(data)   //not doing anything...
-                console.log(data)
-
+            var latValue = data.city.coord.lat
+            var lonValue = data.city.coord.lon
+            var apiSearchLatLon = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latValue + "&lon=" + lonValue + "&appid=" + APIkey + "&units=imperial"
         
 
+            console.log(data)
 
-            //     for (var i=0; i<data.length; i++) {
-              
-            // cityName = data[i].city.name
-           
-            // console.log(cityName)
+        
+        //use lat lon to call second API
+        
+        fetch(apiSearchLatLon)
+        .then(function (response) {
+           return response.json()
+        })
+        .then(function (data2){
     
-            // } 
+           
+        
+           //get date of searched city
+           d = new Date()
+           localTime = d.getTime()
+           localOffset = d.getTimezoneOffset() * 60000
+           utc = localTime + localOffset
+           var searchCity = utc + (1000 * data.city.timezone)
+           nd = (new Date(searchCity)).toLocaleDateString('en-US')
 
+
+           //empty weather card and fill with input city 
+
+            $("#weather").empty();
+            resultCard = $("#searchResults").append("<div class='card' 'bg-light' 'text-dark' 'mb-3', 'p-3'></div>")
+            cardBody = $("#weather").append('<div class="card body"></div>')
+            $(cardBody).append('<h1 class = "card-title">' + data.city.name + '  (' + nd + ') <img src="http://openweathermap.org/img/wn/' + data2.current.weather[0].icon + '@2x.png" alt="Weather icon"></h2>'
+            // $(cardBody).append('<img src="http://openweathermap.org/img/wn/' + data2.current.weather[0].icon + '@2x.png" alt="Weather icon">'
+                                 +"<p class='card-text'> Temperature: "  + data2.current.temp + " °F <br/></p>"
+                                 +"<p class='card-text'> Humidity: " + data2.current.humidity + " % <br/></p>"
+                                 +"<p class='card-text'> Wind Speed: " + data2.current.wind_speed + " MPH </p>"
+                                 +"<button type='button' class='btn' id='uviBtn'> UV Index: " + data2.current.uvi + " </button>")
+
+                                 // change color depending on uv value
+        if (data2.current.uvi < 3) {
+            $("#uviBtn").addClass("btn-success")
+        }
+          else if (data2.current.uvi < 7) {
+            $("#uviBtn").addClass("btn-warning");
+          }
+          else {
+            $("#uviBtn").addClass("btn-danger");
+          }
+    
+            
+
+                // $("#searchResults").append(data)   
+                console.log(data2)
+
+        //five day forcast
+        //  $("#fiveDay").append("<div class = 'col-3'><h1 class = 'card-title'> " + nd + " </h1></div>")   
+         $("#fiveDay").append("<p class ='card-text>" + data2.current.wind_speed + "</p></div>")   
+
+         
           })  
           
           
-        
+        })
 
-        //ASK ABOUT JSON
-            // var fiveDayForcast = getForecast(searchValue))
+      
     }
-
 
 })
 
